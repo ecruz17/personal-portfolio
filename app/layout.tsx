2e252def0d561bbfab28d9c60266d4d0a0f9d543
ecruz17 <emircruz.m@gmail.com>
2024-07-03 16:22:13 -0600

    implemented thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import { Kanit } from 'next/font/google'
 import './globals.css'
 import {Footer} from '../components/Footer'
 import ResponsiveNav from '@/components/ResponsiveNav'
+import Head from 'next/head'
 
 const kanit = Kanit({ subsets: ['latin'], weight: ['200', '300', '400', '600'] })
 
 export const metadata: Metadata = {
-  title: "ecruz portfolio",
-  description: "Landing page for the Ecruz's Portfolio",
+  title: "Emir Cruz - Software Engineer",
+  description: "Landing page for the Ecruz's Portfolio"
 }
 
 export default function RootLayout({
@@ -17,6 +18,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <>
+      <Head>
+        <title>Your Page Title</title>
+        <meta property="og:image" content="https://ecruz.dev/thumbnail.png" />
+        <meta property="og:url" content="https://ecruz.dev" />
+        <meta name="twitter:card" content="/thumbnail.png" />
+      </Head>
     <html lang="en">
       <body className={`${kanit.className} flex flex-col min-h-screen bg-[#1E2020]`}>
         <ResponsiveNav />
@@ -25,6 +33,7 @@ export default function RootLayout({
         }       
         <Footer />
       </body>
-    </html>
+      </html>
+    </>
   )
 }
